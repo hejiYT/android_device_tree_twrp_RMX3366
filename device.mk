99a0b914231d602348f3f821fb9fde4a78f9c76d
hejiYT <hexiaoshan520@gmail.com>
2022-05-19 05:19:48 +0800

    new

--- device.mk
+++ device.mk
@@ -5,6 +5,10 @@ LOCAL_PATH := device/realme/RMX3366
 
 # define hardware platform
 PRODUCT_PLATFORM := kona
-   
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
